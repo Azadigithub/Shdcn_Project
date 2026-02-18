49cd9af3fe059e3357d83d8977b9f7726020cb2c
Ali <aliazadigithub@gmail.com>
2026-02-18 16:08:43 +0330

    Feat : React router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { useRoutes } from "react-router-dom";
+const routes = [
+  { path: "/", element: <h1>home</h1> },
+  // {path:'/' , element : <></>},
+  // {path:'/' , element : <></>},
+];
 function App() {
+  const router = useRoutes(routes);
 
   return (
     <>
-      <div className=''></div>
-
+      <div className=""></div>
+      {router}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
